559508de2344f1a15597be7f34e5be20304ad8c0
wxodi0 <taeyang127.0.0.1@gmail.com>
2023-09-13 22:19:42 +0900

    LOVE

--- index.js
+++ index.js
@@ -4,19 +4,39 @@ import { OrbitControls } from "https://unpkg.com/three@0.126.1/examples/jsm/cont
 //scene
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1 , 1000 );
 
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff80 } );
+const shape = new THREE.Shape();
+const x = -2.5;
+const y = -5;
+shape.moveTo(x + 2.5, y + 2.5);
+shape.bezierCurveTo(x + 2.5, y + 2.5, x + 2, y, x, y);
+shape.bezierCurveTo(x - 3, y, x - 3, y + 3.5, x - 3, y + 3.5);
+shape.bezierCurveTo(x - 3, y + 5.5, x - 1.5, y + 7.7, x + 2.5, y + 9.5);
+shape.bezierCurveTo(x + 6, y + 7.7, x + 8, y + 4.5, x + 8, y + 3.5);
+shape.bezierCurveTo(x + 8, y + 3.5, x + 8, y, x + 5, y);
+shape.bezierCurveTo(x + 3.5, y, x + 2.5, y + 2.5, x + 2.5, y + 2.5);
+
+const extrudeSettings = {
+  steps: 2,  
+  depth: 2,  
+  bevelEnabled: true,  
+  bevelThickness: 1,  
+  bevelSize: 1,  
+  bevelSegments: 2,  
+};
+
+const geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+const material = new THREE.MeshBasicMaterial( { color: 0x00FEFF } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 5;
+camera.position.z = 100;
 
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.minDistance = 1;
@@ -24,10 +44,12 @@ controls.maxDistance = 500;
 
 const animate = function () {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.11;
+  cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   controls.update();
   renderer.render(scene, camera);
 };
+
+
 
 animate();
